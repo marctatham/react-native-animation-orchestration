2025e9023c547d655ba276a28b59e653adc867b8
Marc Tatham <marctatham@gmail.com>
2023-05-03 15:15:13 +0200

    broke screen contents out into DefaultScreen
    It's all to throw away anyway, but forms a good base

--- App.tsx
+++ App.tsx
@@ -6,16 +6,10 @@
  */
 
 import React from "react";
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
+import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
-import Section from "./src/Section";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import DefaultScreen from "./src/DefaultScreen";
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
@@ -30,38 +24,9 @@ function App(): JSX.Element {
         barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <DefaultScreen />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: "700",
-  },
-});
 
 export default App;
---
+++ src/DefaultScreen.tsx
@@ -0,0 +1,53 @@
+import { ScrollView, StyleSheet, Text, useColorScheme, View } from "react-native";
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from "react-native/Libraries/NewAppScreen";
+import React from "react";
+import Section from "./Section";
+
+function DefaultScreen(): JSX.Element {
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={backgroundStyle}>
+      <Header />
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }}>
+        <Section title="Step One">
+          Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+          screen and then come back to see your edits.
+        </Section>
+        <Section title="See Your Changes">
+          <ReloadInstructions />
+        </Section>
+        <Section title="Debug">
+          <DebugInstructions />
+        </Section>
+        <Section title="Learn More">
+          Read the docs to discover what to do next:
+        </Section>
+        <LearnMoreLinks />
+      </View>
+    </ScrollView>
+  );
+}
+
+export default DefaultScreen;
+
+const styles = StyleSheet.create({
+  highlight: {
+    fontWeight: "700",
+  },
+});
